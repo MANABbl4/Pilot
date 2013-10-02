555f3896e40d3761ce4ac9b3c31567e7729ae515
MANABbl4 <vitaly.kachan@gmail.com>
2013-10-02 09:52:25 +0200

    Added plane 514x514 verts.

--- Assets/Scripts/Earth.cs
+++ Assets/Scripts/Earth.cs
@@ -3,6 +3,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 public class Earth : SingletonGameObject<Earth>
 {
@@ -73,28 +74,131 @@ public class Earth : SingletonGameObject<Earth>
 
 	private void InitQuads()
 	{
-        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		int width = 514;
+		int height = 514;
+
+		List<List<ushort>> data = LoadTerrain("Assets/Textures/Earth_bump/neg_z/0_0_0.raw", width, height);
+
+		Object resource = GameObject.Instantiate(Resources.Load("Prefabs/plane"));
+		if (resource != null)
+		{
+			GameObject plane = resource as GameObject;
+			plane.SetActive(true);
+
+			if (plane != null)
+			{
+				plane.name = resource.name;
+
+				plane.transform.rotation = Quaternion.identity;
+				plane.transform.position = Vector3.zero;
+				plane.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+				MeshFilter mf = plane.GetComponent<MeshFilter>();
+				if (mf != null)
+				{
+					Mesh m = mf.mesh;
+					Vector3[] vertices = m.vertices;
+
+					for (int i = 0; i < height; ++i)
+					{
+						for (int j = 0; j < width; ++j)
+						{
+							vertices[i * width + j] += (Vector3.up) * data[i][j] / ushort.MaxValue;
+						}
+					}
+
+					m.vertices = vertices;
+					mf.sharedMesh = m;
+					mf.sharedMesh.RecalculateNormals();
+					mf.sharedMesh.RecalculateBounds();
+				}
+				else
+				{
+					Log("mf = null");
+					MeshFilter[] mfs = plane.GetComponentsInChildren<MeshFilter>();
+					int vert = 0;
+					foreach (MeshFilter mf1 in mfs)
+					{
+						Mesh m = mf1.mesh;
+						Vector3[] vertices = m.vertices;
+
+
+						for (int i = 0; i < vertices.Length; ++i)
+						{
+							int j = 0;
+							int k = 0;
+
+							j = Mathf.FloorToInt((i + vert) / data.Count);
+							k = i + vert - j * data.Count;
+
+							vertices[i] += 10 * (Vector3.up) * data[j][k] / ushort.MaxValue;
+						}
+
+						vert += vertices.Length;
+
+						m.vertices = vertices;
+						mf1.sharedMesh = m;
+						mf1.sharedMesh.RecalculateNormals();
+						mf1.sharedMesh.RecalculateBounds();
+					}
+				}
+			}
+			else
+			{
+				Log("prefab = null");
+			}
+		}
+		else
+		{
+			Log("resource = null");
+		}
+
+        /*GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Plane);
         MeshFilter mf = obj.GetComponent<MeshFilter>();
-        mf.sharedMesh = MeshUtils.Plane(Vector3.zero, Vector3.right * 1, Vector3.up * 1, 103, 103);
+        Mesh m = MeshUtils.Plane(Vector3.zero, Vector3.right * 1, Vector3.up * 1, 103, 103);
+		Vector3[] vertices = m.vertices;
+
+		for(int i = 0; i < 103; ++i)
+		{
+			for(int j = 0; j < 103; ++j)
+			{
+				vertices[i * 103 + j] += (-Vector3.forward) * data[i][j] / 6553;
+			}
+		}
+
+		m.vertices = vertices;
+		mf.sharedMesh = m;
         mf.sharedMesh.RecalculateNormals();
         mf.sharedMesh.RecalculateBounds();
         obj.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         obj.transform.position = Vector3.zero;
-        obj.transform.rotation = Quaternion.identity;
+        obj.transform.rotation = Quaternion.identity;*/
+	}
+
+	private List<List<ushort>> LoadTerrain(string fileName, int width, int height)
+	{
+		List<List<ushort>> data = new List<List<ushort>>();
+
+		using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+		{
+			for (int i = 0; i < height; ++i)
+			{
+				List<ushort> row = new List<ushort>();
+
+				for (int j = 0; j < width; ++j)
+				{
+					row.Add(reader.ReadUInt16());
+				}
+
+				data.Add(row);
+			}
+		}
+
+		return data;
 	}
 
 	private void Update()
 	{
-		/*foreach (GameObject obj in m_airports)
-		{
-			Vector3 normal = obj.transform.position - Earth.Instance().GetCenter();
-			Vector3 forward = Vector3.up;
-			Vector3 left = Vector3.right;
-
-			Vector3.OrthoNormalize(ref normal, ref forward, ref left);
-			Debug.DrawLine(obj.transform.position, obj.transform.position + Camera.mainCamera.transform.rotation * (Vector3.right * 20), Color.blue);
-			//Debug.DrawLine(obj.transform.position, obj.transform.position + left.normalized * 20, Color.red);
-		}*/
 	}
 
 	[SerializeField]
---
+++ Assets/Textures/Earth_bump/Earth-bump-readme.txt
@@ -0,0 +1,24 @@
+Bump (elevation) texture of Earth for SpaceEngine v0.95
+02 mar 2012
+
+1. Description
+-------------------------------------------------------------------------------
+
+The pack contains elevation map in the subfolder "Bump". It has 7 levels of
+514 x 514 textures with a total resolution of 131072 x 65536 (equator x
+meridian). This has a surface resolution of approximately 305 meters per pixel.
+All textures are in lossless png format with high 16-bit precision.
+
+2. Installation
+-------------------------------------------------------------------------------
+
+Just unpack this archive into "\SpaceEngine\data\textures\planets" folder. Make
+sure textures are unpacked to correct subfolders ("Earth\Bump\"). To save disk
+space, you may remove old folder "Earth\Bump" from the "planets" folder -
+SpaceEngine will ignore jpeg files in it later on.
+
+3. Credit
+-------------------------------------------------------------------------------
+
+All images credit to NASA. Original data - Shuttle Radar Topography Mission
+(SRTM)
